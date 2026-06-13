b1264d73643e85a7993c3181e9049ae14f3ed470
FraserLB <122133384+FraserLB@users.noreply.github.com>
2026-06-13 12:16:12 -0700

    Create Player.h

---
+++ Project2/Player.h
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+#include "Deck.h"
+#include "Hand.h"
+using namespace std;
+
+class Player {
+    protected:
+        Hand hand;
+        string name;
+    public:
+        Player(string n);
+        virtual void playTurn(Deck &deck) = 0;  //Virtual function
+        void addCard(Card c);
+        int getTotal();
+        string getName();
+};
+
+#endif
