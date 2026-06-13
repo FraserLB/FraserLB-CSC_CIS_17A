0467a02109e9ed4426841a23970989e9644edc14
FraserLB <122133384+FraserLB@users.noreply.github.com>
2026-06-13 12:28:38 -0700

    Update HumanPlayer.cpp

--- Project2/HumanPlayer.cpp
+++ Project2/HumanPlayer.cpp
@@ -7,15 +7,17 @@ HumanPlayer::HumanPlayer(string n) : Player(n) {
 void HumanPlayer::playTurn(Deck &deck) {
     char choice;
     do {
+        Card c;
         cout << "Hit (h) or Stand (s): ";
         cin >> choice;
         if(choice == 'h') {
             addCard(deck.deal());
             cout << "New Hand: " << endl;
             for(int i = 0; i < hand.getCount(); i++) {
-                Card c = hand.getCard(i);
+                c = hand.getCard(i);
                 cout << c.getName() << " (" << c.getValue() << ")" << endl;
             }
+            cout << "Total: " << getTotal() << endl;
         }
     } while(choice != 's' && getTotal() <= 21);
 }
